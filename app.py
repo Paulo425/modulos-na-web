fb136cb3d4f888b5fe41bc1627c52799319cf8b6
Paulo425 <phoenixcoworking@gmail.com>
2025-06-08 19:34:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,16 +18,14 @@ os.makedirs(password_dir, exist_ok=True)
 os.makedirs(dir_log_projeto, exist_ok=True)
 
 admin_path = os.path.join(password_dir, "admin.json")
-if not os.path.exists(admin_path):
-    print("⚙️ Criando admin.json no ambiente atual...")
-    admin_data = {
-        "usuario": "admin",
-        "senha_hash": "pbkdf2:sha256:600000$MUVq0v03EaDI0pRV$bb9a268b95b1baf86c6277ef1cdadcc3c7c443b2bc9608c48a574e8dc1de76c0"
-    }
-    with open(admin_path, 'w', encoding='utf-8') as f:
-        json.dump(admin_data, f, indent=2)
-else:
-    print("✅ admin.json já existe")
+admin_data = {
+    "usuario": "admin",
+    "senha_hash": "pbkdf2:sha256:600000$MUVq0v03EaDI0pRV$bb9a268b95b1baf86c6277ef1cdadcc3c7c443b2bc9608c48a574e8dc1de76c0"
+}
+with open(admin_path, 'w', encoding='utf-8') as f:
+    json.dump(admin_data, f, indent=2)
+print("🛠️ admin.json recriado com senha 1234")
+
 
         
 def carregar_usuarios():
