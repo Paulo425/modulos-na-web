05c9d50db5b599bfc70ee6dc7ab43121b518eb81
Paulo425 <phoenixcoworking@gmail.com>
2025-06-17 12:41:17 -0300

    Update executar_memorial_azimute_jl.py

--- executaveis/executar_memorial_azimute_jl.py
+++ executaveis/executar_memorial_azimute_jl.py
@@ -15,7 +15,7 @@ def executar_memorial_jl(proprietario, matricula, descricao, caminho_salvar, dxf
             dxf_limpo_path = os.path.join(caminho_salvar, f"DXF_LIMPO_{matricula}.dxf")
             dxf_limpo_path, ponto_az = limpar_dxf_e_inserir_ponto_az(dxf_path, dxf_limpo_path)
 
-            doc, lines, arcs, perimeter, area, ponto_az = get_document_info_from_dxf(dxf_limpo_path)
+            doc, lines, arcs, perimeter_dxf, area_dxf = get_document_info_from_dxf(dxf_limpo_path)
 
             if not doc or not ponto_az:
                 raise ValueError("Erro ao processar o DXF ou ponto Az não encontrado.")
@@ -43,8 +43,8 @@ def executar_memorial_jl(proprietario, matricula, descricao, caminho_salvar, dxf
                 excel_file_path=excel_output,
                 template_path=template_path,
                 output_path=docx_path,
-                perimeter_dxf=perimeter,
-                area_dxf=area,
+                perimeter_dxf=perimeter_dxf,
+                area_dxf=area_dxf,
                 Coorde_E_ponto_Az=ponto_az[0],
                 Coorde_N_ponto_Az=ponto_az[1],
                 azimuth=azimuth,
