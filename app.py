ab3405bc8102c607113376764332a6739aeabc68
Paulo425 <phoenixcoworking@gmail.com>
2025-08-02 14:33:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -796,7 +796,8 @@ from executaveis_avaliacao.main import gerar_relatorio_avaliacao_com_template
 
 @app.route("/avaliacoes", methods=["GET", "POST"])
 def gerar_avaliacao():
-
+    
+    logger = logging.getLogger(__name__)  # ← ajuste definitivo aqui!
     logger.debug("🚀 Iniciando rota gerar_avaliacao()")
 
 
