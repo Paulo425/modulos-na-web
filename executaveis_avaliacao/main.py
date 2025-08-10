5ff704be0a4a11e8f51474e93fd36fe337589084
Paulo425 <phoenixcoworking@gmail.com>
2025-08-10 18:40:24 -0300

    Update main.py

--- executaveis_avaliacao/main.py
+++ executaveis_avaliacao/main.py
@@ -5157,7 +5157,7 @@ def calcular_detalhes_amostras(dataframe_amostras_validas, dados_avaliando, fato
                     fator_item_comparativo = 1 / math.pow(distancia_amostra, 1/10)
                     fator_bem_avaliando = 1 / math.pow(distancia_avaliando, 1/10)
                     fator_localizacao_calculado = fator_bem_avaliando / fator_item_comparativo
-                    if fator_localizacao_calculado > 1.40:
+                if fator_localizacao_calculado > 1.40:
                         fator_localizacao_calculado = 1.40
                     elif fator_localizacao_calculado < 0.50:
                         fator_localizacao_calculado = 0.50
